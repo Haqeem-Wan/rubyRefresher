d3445001544d0c367e5a21180c365cb63f10c3c5
Haqeem Wan <haqeemwan@gmail.com>
2022-11-27 23:12:01 +0800

    Outline, Comments, I/O, Variables, Data Type Conversion

--- rubyRefresher.rb
+++ rubyRefresher.rb
@@ -1,1 +1,137 @@
-puts "First commit test"
+=begin
+     
+    Welcome to rubyRefresher! 
+    
+        The "Refresher" project series is a series dedicated to teach you quick and simple 
+        refreshers on how different programming languages works on their syntax!
+
+        This was made as the creator, Wan Nashrul Haqeem, tended to not be able to remember
+        every language at once and often had to google or revise on syntax when working on
+        a language he hasn't worked with in a while! I'm sure this is an issue a lot of you
+        would be able to relate to! Thus, the Refresher series was born!
+
+        This Refresher project is dedicated to the Ruby programming language!
+        Project Start   : 27th November 2022
+        Project End     : Pending
+
+=end
+
+=begin
+
+    Important Information!
+
+        The Refresher series assumes you have knowledge on the fundamentals of coding, in which it
+        will only show the syntax of them, and only provide explanations if necessary
+
+        Explanations will be noted with a '###' string at the front of the sentence!
+        EG : 
+            ~ This is an important statement!
+
+        Pro Tips!
+            Ctrl+F to quickly find which syntax you would like a refresher on!
+            
+            If you're using Visual Studio Code, you can use the command CTRL+/ to to
+            quickly uncomment and run the code!
+
+=end
+
+
+# --------------------------------------------------------------------------------------------------------
+
+# Topic 0 : Comments!
+
+    # Comments are useful for leaving information in your code without executing anything! Kinda like this!
+
+    # Single line comments
+
+        # You write a comment with a '#' char at the front!
+
+    # Multi line comments
+
+    ### Multiline comments in Ruby tend to break once indented, so be careful on that
+
+=begin
+        =begin
+            You can write multi-line comments with "=begin" at the front and closing with "=end"
+        =end
+=end
+
+# --------------------------------------------------------------------------------------------------------
+
+# Topic 1 : Input and Output!
+
+    # Input
+        ### Can be combined with Topic 3 to convert data types accordingly
+
+
+        # Syntax :
+            # variable = gets
+            # variable = gets.to_i  # See Topic 3 for explanation on "to_i"
+
+
+    # Method 1 : print
+
+        ### using print will NOT add newline characters at the end of each print
+
+        # Syntax :
+            # print "This will be printed to the terminal! => "
+            # print "This won't be sent to the next line!\n"
+
+    # Method 2 : puts
+
+        ### using puts WILL add newline characters at the end of each print
+
+        # Syntax :
+            # puts "This will be printed to the terminal!"
+            # puts "This WILL be sent to the next line!"
+
+# --------------------------------------------------------------------------------------------------------
+
+# Topic 2 : Variables!
+
+    ### Defining variables in Ruby do NOT need to be preceeded by a Data Type!
+    ### Ruby will instead assign the data type itself following the input given to the variable
+    ### Because of this, variable initialization does not exist in Ruby, it is only variable declaration
+    ### Convention is to start with a lower case letter, and use '-' or '_' between words
+
+    # Syntax :
+        # variable_1 = "String"     # Data Type assigned : string
+        # variable_2 = 123          # Data Type assigned : integer
+        # variable_3 = 'h'          # Data Type assigned : character
+        # variable_4 = 1.23         # Data Type assigned : float
+
+# --------------------------------------------------------------------------------------------------------
+
+# Topic 3 : Converting Data Types!
+
+    ### Converts Data  Types
+
+    # Syntax :
+        # variable.to_i             # Converted to integer
+        # variable.to_s             # Converted to string
+        
+# --------------------------------------------------------------------------------------------------------
+
+# --------------------------------------------------------------------------------------------------------
+
+# --------------------------------------------------------------------------------------------------------
+
+# --------------------------------------------------------------------------------------------------------
+
+# --------------------------------------------------------------------------------------------------------
+
+# --------------------------------------------------------------------------------------------------------
+
+# --------------------------------------------------------------------------------------------------------
+
+# --------------------------------------------------------------------------------------------------------
+
+# --------------------------------------------------------------------------------------------------------
+
+# --------------------------------------------------------------------------------------------------------
+
+# --------------------------------------------------------------------------------------------------------
+
+# --------------------------------------------------------------------------------------------------------
+
+# --------------------------------------------------------------------------------------------------------
---
+++ tempCodeRunnerFile.rb
@@ -0,0 +1,2 @@
+print "This will be printed to the terminal! => "
+            print "This won't be sent to the next line!\n"
